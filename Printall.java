4dae179cdfd0f9d3a6e166e224f679434e6943d5
Yashasvi <yashasvichilankar27@gmail.com>
2023-02-16 16:45:01 +0530

    Print all valid data type

---
+++ Printall.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+class Printall
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+
+System.out.println("Enter any String    ");
+String s=sc.nextLine();
+
+System.out.println(""+"Enter any Integer   ");
+int num=sc.nextInt();
+
+System.out.println("Enter any Double     ");
+double d=sc.nextDouble();
+
+System.out.println("Enter any float     ");
+float f=sc.nextFloat();
+
+//System.out.println("Enter any character  ");
+//char c=sc.nextchar();
+
+}
+}
+
+
+
+
+
+
+
+
+
+
