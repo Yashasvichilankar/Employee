bd93f9b04317dbeba1b6b3f2ea2a3cfb5636425f
Yashasvi <yashasvichilankar27@gmail.com>
2023-02-20 11:29:44 +0530

    Adding file sum.java

---
+++ Sum.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class Sum
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+
+System.out.println("Input any Integer between 0 to 1000: ");
+int a=sc.nextInt();
+
+System.out.println("The sum of all digits in "+a+" is ");
+
+}
+}
