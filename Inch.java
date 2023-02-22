d04027059b63d62071879de5e3b6aacfa9b029b1
Yashasvi Chilankar <125111823+Yashasvichilankar@users.noreply.github.com>
2023-02-22 22:39:23 +0530

    Update Inch.java

--- Inch.java
+++ Inch.java
@@ -3,10 +3,11 @@ class Inch
 {
 public static void main(String args[])
 {
+  
 Scanner sc=new Scanner(System.in);
 System.out.println("Enter any value in inches  ");
 float inch=sc.nextFloat();
-
+  
 double meter;
 meter=inch*0.0254;
 
